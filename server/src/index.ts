f1b3d3529683fa5a6fc7cd9e97f94281603b7b7f
ahsan <m.ahsan8588@gmail.com>
2025-11-16 18:49:45 +0500

    port

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/trees', treeRoutes);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
 async function start() {
   try {
